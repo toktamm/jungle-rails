69b69f9b0bc501c56976a6192579b9d0748901fe
Toktam <ttmohebbi@gmail.com>
2020-11-14 09:09:37 +0000

    add admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,12 @@ class Admin::DashboardController < ApplicationController
   include HttpAuthConcern
 
   def show
+
+    @products_count = Product.count
+    @categories_count = Category.count
+    @products_per_category = Category.select(:id, :name).distinct.map do |category|
+      {id: category[:id], name: category[:name], count: category.products.count}
+    end
+
   end
 end
