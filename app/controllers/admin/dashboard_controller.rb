9d6d291025eb68e3da942ba8b002a3b42c49f194
Toktam <ttmohebbi@gmail.com>
2020-11-14 08:05:57 +0000

    create concern module for authentication

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class Admin::DashboardController < ApplicationController
   
-  http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
-  
+  include HttpAuthConcern
+
   def show
   end
 end
