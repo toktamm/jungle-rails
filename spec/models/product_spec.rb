f26ed789ee79234aab09baca6bea07342e43c688
Toktam <ttmohebbi@gmail.com>
2020-11-19 05:05:09 +0000

    add product validation specs

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,31 +2,67 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    context 'products' do
-      before do
-        @category = Category.find_or_create_by(name: 'TMNT')
-        # @category_id = @category.id
-        @original_count = Product.count
-        # puts Category.count
-      end
-      # test by creating a product with all entries but
-      # the one that you are testing - check errors
-      # which makes it five examples
-      # test that @product can be saved
-      it 'gets saved' do
-        @splinter = Product.new({
-          name: 'Splinter',
-          price: 2500,
-          quantity: 2,
-          category_id: @category.id
-        })
-        @splinter.save
-        # puts ("product #{@splinter.inspect}")
 
-        # check if the item was added to the table
-        expect(Product.count).not_to eq(@original_count)
-      end
+    before do
+      @category = Category.create(name: 'Games')
     end
+
+    it 'saves successfully when all fields are set' do
+      @product = Product.new(
+        name: 'Catan', 
+        price: 100, 
+        quantity: 20, 
+        category_id: @category[:id]
+      )
+      @product.save
+      expect(Product.all).to include(@product)
+    end
+
+    it 'returns an error when the name is blank' do
+      @product = Product.new(
+        name: nil, 
+        price: 100, 
+        quantity: 20, 
+        category_id: @category[:id]
+      )
+      @product.save
+      expect(@product.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it 'returns an error when the price is blank' do
+      @product = Product.new(
+        name: 'Catan', 
+        price: nil, 
+        quantity: 20, 
+        category_id: @category[:id]
+      )
+      @product.save
+      expect(@product.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it 'returns an error when the quantity is blank' do
+      @product = Product.new(
+        name: 'Catan', 
+        price: 100, 
+        quantity: nil, 
+        category_id: @category[:id]
+      )
+      @product.save
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end
+
+    it 'returns an error when the category_id is blank' do
+      @product = Product.new(
+        name: 'Catan', 
+        price: 100, 
+        quantity: 20, 
+        category_id: nil
+      )
+      @product.save
+      expect(@product.errors.full_messages).to include("Category can't be blank")
+
+    end
+
   end
 end
 
