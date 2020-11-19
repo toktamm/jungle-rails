a2578cda8f15b22df90751e3cdb2e4cb9de00fc4
Toktam <ttmohebbi@gmail.com>
2020-11-19 07:56:11 +0000

    add first set of specs to validate register inputs

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  validates :password, presence: true, length: { minimum: 7 }
+  validates :password_confirmation, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :first_name, presence: true
+  validates :last_name, presence: true  
+
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,109 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+
+    it 'creates successfully when all the required fields are filled, the passwords match and password length is as expected' do
+      @user = User.create(
+        first_name: "Toktam",
+        last_name: "Mohebbi",
+        email: "example@domain.com",
+        password: "123",
+        password_confirmation: "123"
+      )
+      expect(User.all).to include(@user)
+    end
+
+    it 'returns an error when password is empty' do
+      @user = User.create(
+        first_name: "Toktam",
+        last_name: "Mohebbi",
+        email: "example@domain.com",
+        password: nil,
+        password_confirmation: "123"
+      )
+       expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+
+    it 'returns an error when password_confirmation is empty' do
+      @user = User.create(
+        first_name: "Toktam",
+        last_name: "Mohebbi",
+        email: "example@domain.com",
+        password: "123",
+        password_confirmation: nil
+      )
+      expect(@user.errors.full_messages).to include("Password confirmation can't be blank")
+    end
+
+    it 'returns an error when email is empty' do
+      @user = User.create(
+        first_name: "Toktam",
+        last_name: "Mohebbi",
+        email: nil,
+        password: "123",
+        password_confirmation: "123"
+      )
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+
+    it 'returns an error when first_name is empty' do
+      @user = User.create(
+        first_name: nil,
+        last_name: "Mohebbi",
+        email: "example@domain.com",
+        password: "123",
+        password_confirmation: "123"
+      )
+      expect(@user.errors.full_messages).to include("First name can't be blank")
+    end
+
+    it 'returns an error when last_name is empty' do
+      @user = User.create(
+        first_name: "Toktam",
+        last_name: nil,
+        email: "example@domain.com",
+        password: "123",
+        password_confirmation: "123"
+      )
+      expect(@user.errors.full_messages).to include("Last name can't be blank")
+    end
+
+    it 'returns an error when password and password_confirmation do not match' do
+      @user = User.create(password: "123", password_confirmation: "123")
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    it 'returns an error when the email is already registered (not case sensitive)' do
+      @user1 = User.create(
+        first_name: "Toktam",
+        last_name: "Mohebbi",
+        email: "example@domain.com",
+        password: "123",
+        password_confirmation: "123"
+      )
+      @user2 = User.create(
+        first_name: "Toktam",
+        last_name: "Mohebbi",
+        email: "example@domain.com",
+        password: "123",
+        password_confirmation: "123"
+      )
+      expect(@user2.errors.full_messages).to include("Email has already been taken")
+    end
+
+    it "returns an error when the password's length is less than 7" do
+      @user = User.create(password: "12", password_confirmation: "12")
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 7 characters)")
+    end
+  end
+
+
+  describe '.authenticate_with_credentials' do
+    # examples for this class method here
+  end
+
+end
+
+# email uniqueness checks with current database. It does not create a uniqueness constraint in the database, so it may happen that two different database connections create two records with the same value for a column that you intend to be unique. To avoid that, you must create a unique index on both columns in your database. 
+
